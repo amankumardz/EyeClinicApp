16381ba62d79de72b5331eb4aa703fdf4bc0c410
Aman Kumar <aman.kumar@intelegencia.com>
2026-03-29 01:06:37 +0530

    commit

---
+++ Migrations/20260328181807_EnsureStartupSeedAndAuthFix.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EyeClinicApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnsureStartupSeedAndAuthFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
