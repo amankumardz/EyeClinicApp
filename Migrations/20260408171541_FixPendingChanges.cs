7ea93e2af3ddf4ba2c8783d69e2191681e357da4
Aman Kumar <aman.kumar@intelegencia.com>
2026-04-08 22:50:09 +0530

    commit

---
+++ Migrations/20260408171541_FixPendingChanges.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EyeClinicApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_UserId",
+                table: "Appointments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_UserId",
+                table: "Appointments",
+                column: "UserId");
+        }
+    }
+}
